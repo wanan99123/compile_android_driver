5e8f836ef73e50e91fc613b028f423fdf3fa387f
wanan99123 <1632605321@qq.com>
2026-02-21 23:03:54 +0800

    Update gs_mem.h

--- code/gs_mem.h
+++ code/gs_mem.h
@@ -2,28 +2,17 @@
 #define GS_MEM_H
 
 #include <linux/types.h>
-#include <linux/mm.h>
-#include <linux/fs.h>
-#include <linux/kernel.h>
-#include <linux/slab.h>
-#include <linux/sched.h>
-#include <linux/pid.h>
-#include <linux/netlink.h>
-#include <asm/io.h>
-#include <linux/highmem.h>
-#include <linux/uaccess.h>
-#include <linux/string.h>
 
-#define MAX_ADDR_LEN        128
 #define NETLINK_CUSTOM_PROTOCOL 20
+#define MAX_ADDR_LEN 256
 
 struct process_info {
-    pid_t               pid;
-    size_t              virt_addr;
-    size_t              len;
-    void __user         *base;
-    int                 type;
-    char                module_name[MAX_ADDR_LEN];
+    pid_t pid;
+    size_t virt_addr;
+    size_t len;
+    void *base;
+    int type;
+    char module_name[MAX_ADDR_LEN];
 };
 
-#endif
+#endif // GS_MEM_H
